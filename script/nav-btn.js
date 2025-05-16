447ae2bc992995fab261dacb6cc592b1e23ca1e9
kacharles <koenigalfredcharles@gmail.com>
2025-05-16 02:29:00 +0000

    Homepage Update

--- script/nav-btn.js
+++ script/nav-btn.js
@@ -1,6 +1,6 @@
 
 const menuBtn = document.querySelector("#menu-btn"); 
-const navMenu = document.querySelector(".nav-menu");
+const navMenu = document.querySelector("#animate-me");
 menuBtn.addEventListener("click", () => { 
     menuBtn.classList.toggle("activate");
     navMenu.classList.toggle("activate");

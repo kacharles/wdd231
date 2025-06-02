e58014ac0dad4e818148a9f90e5c58ccdc8d0c4d
kacharles <koenigalfredcharles@gmail.com>
2025-06-02 22:56:04 +0000

    Update Homepage

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -3,6 +3,7 @@
 const menuBtn = document.querySelector("button"); 
 const mobileMenu = document.querySelector("#mobile-menu"); 
 
+
 menuBtn.addEventListener("click", () => {
     menuBtn.classList.toggle("activate");
     mobileMenu.classList.toggle("activate");
@@ -15,6 +16,72 @@ const lastModified = document.querySelector("#lastModified");
 
 copyright.innerHTML = `&copy; ${new Date().getFullYear()} | Charles Alfred Koenig | WDD 231 Web Frontend Development</span>`;
 lastModified.textContent = "Last Updated: " + document.lastModified;
+
+async function member(){
+    const membership = "https://kacharles.github.io/wdd230/chamber/data/members.json";
+    const response = await fetch(membership);
+    try{
+        if (response.ok){
+            const data = await response.json();
+            // console.log(data)
+            spotlight(data);
+        }
+        else{
+            throw Error(await response.text());
+        }
+    }
+    catch (error){
+        console.log(error);
+    }
+}
+member();
+function spotlight(member){
+    // const spotlight = document.querySelector("#spotlights");
+    const already = new Set();
+    
+    for (let index = 0; index < 3; index++) {
+        const randomIndex = Math.floor(Math.random() * member['members'].length);
+        const company = member['members'][randomIndex];
+        const membership = company['ml']; 
+        
+        if (membership === "Silver" || membership === "Gold"){
+            if (already.has(randomIndex)){
+                index--;
+                continue;
+            }
+        already.add(randomIndex);
+        const main = document.querySelector(".grid"); 
+        let sectionEle = document.createElement("section"); 
+        let portrait = document.createElement("img"); 
+        let heading = document.createElement("h3"); 
+        let address = document.createElement("p");
+        let url = document.createElement("p");
+        
+        // console.log(member.website);
+
+        portrait.setAttribute("src", company.image);
+        portrait.setAttribute("alt", company.name);
+
+        heading.textContent = `${company.name}`; 
+        address.textContent = `${company.address}`; 
+        url.textContent = `${company.website}`;
+
+        sectionEle.appendChild(portrait);
+        sectionEle.appendChild(heading); 
+        sectionEle.appendChild(address);
+        sectionEle.appendChild(url); 
+ 
+        main.appendChild(sectionEle);
+        }
+        else{
+            if (index >= 0){
+                index--;
+            }
+        }
+    };
+
+}
+
 
 // Implementing localStorage 
 
